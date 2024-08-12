5f462c65c198749e013263442de13d69a5724db1
Alqama_dev <123807355+Alqama-Shahzad@users.noreply.github.com>
2024-08-12 17:19:54 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import './style.css'
+
+import products from './api/products.json';
+import { showProductContainer } from './api/homeProductsCards';
+
+// - Define a function named `showProductContainer` that takes an array of products as input*/
+
+showProductContainer(products);
